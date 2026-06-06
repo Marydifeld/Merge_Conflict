27c06f6169772c6bcaa4181efd6bddf26341d4f0
Nic <A01278821@tec.mx>
2026-06-06 15:51:21 -0600

    KPI

--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -1,13 +1,14 @@
 import Logo from "../Images/Logo.png";
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 
 function Navbar() {
     return (
       <nav className="navbar">
         <img src={Logo} alt="Logo"/>
         <div className="navbarButtons">
-          <button>Inicio</button>
-          <button>Churn</button>
-          <button>Home</button>
+          <Link to="/mainPage">Main Page</Link>
+          <Link to="/churn">Churn</Link>
+          <Link to="/solutions">Solutions</Link>
         </div>
       </nav>
     );
