43da892399fc2bf59467401d8f4f6523fe550394
Nic <A01278821@tec.mx>
2026-06-07 10:45:25 -0600

    warning link main page

--- frontend/src/Components/ClientWarning.jsx
+++ frontend/src/Components/ClientWarning.jsx
@@ -1,11 +1,17 @@
+import { Link } from 'react-router-dom'
 function ClientWarning({ client }) {
   return (
     <div className="client-card">
       <div className="client-info">
-        <span className="client-name">{client.id}</span>
+        <span className="client-name">{client.id.slice(0, 18) + "..."}</span>
         <span className="client-risk">Risk: {client.riesgo + '%'}</span>
       </div>
-    </div>
+     
+      <Link to={`/details/${client.id}`} className="view-more-link">
+            Ver más
+          </Link>
+</div>
+    
   )
 }
 
